7e7f910a9fac8203e71c980f19e5859c4fe105de
avoleon15 <avoleon15@gmail.com>
2026-03-30 13:43:57 -0600

    Added a new page called transportX, depending on the transport that is clicked, it is the one that the information will be shown

--- src/pages/TransportX.jsx
+++ src/pages/TransportX.jsx
@@ -1,39 +1,88 @@
-import Footer from "../components/Footer/Footer"
-import Navbar from "../components/Navbar/Navbar.jsx"
-import './TransportX.css'
+import { faShip } from '@fortawesome/free-solid-svg-icons';
+import seafreight from "../assets/images/seafreight.jpg";
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import TransportXService from "../components/TransportXService/TransportXService.jsx";
+import './TransportX.css';
 
 function TransportX(){
     return(
-        <>
+        <section id='transportX'>
             <Navbar/>
-            <section className="transportX-header">
-                <div className="transportX-header-box">
-                    <h1>Seafreight</h1>
+            <section id='transportX-header'>
+                <div className="transportX-headerbox1">
+                    <h2>Seafreight</h2>
                     <p>TRANSPORTS</p>
                 </div>
-            </section>
-            <section className="divided-container">
-                <div>
+                <div className="transportX-headerbox2">
+                    <h5>Our customers save cost with our shipping services</h5>
                     <p>Our customers save cost with our shipping services , both full loads FCL, LCL and consolidated . Due to preferential rates with the major shipping companies worldwide, we can offer solutions for shipping to or from Guatemala with rates and transit times very competitive, even for oversized cargo or any other logistics project , taking any need, let us offer our customer support and compliance.</p>
                 </div>
-                <div>
-                    <img src='src/assets/images/airfreight.jpg' alt='airfreight image'></img>
+            </section>
+            <section className='transportX-services'>
+                <section className='transportX-servicescontainer'>
+                    <div className='transportX-servicesbox'>
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    </div>
+                    <div className='transportX-servicesbox'>
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    </div>
+                </section>
+                <div className='transportX-servicesimage'>
+                    <img src={seafreight} alt="" />
                 </div>
             </section>
-            <section className="divided-container inverted-container">
-                <div>
-                    <ul>
-                        <li>uno</li>
-                        <li>dos</li>
-                        <li>tres</li>
-                    </ul>
-                </div>
-                <div>
-                    <img src='src/assets/images/airfreight.jpg' alt='airfreight image'></img>
+            <section className='transportX-services inverted'>
+                <section className='transportX-servicescontainer'>
+                    <div className='transportX-servicesbox'>
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    </div>
+                    <div className='transportX-servicesbox'>
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    <TransportXService
+                    icon={faShip}
+                    title='Import & Export FCL'
+                    description='llorem alkd mani amls mof noanf mlamf noinf nuefafdsm uneuf fmi mail fnmief mof emfoe'
+                    />
+                    </div>
+                </section>
+                <div className='transportX-servicesimage'>
+                    <img src={seafreight} alt="" />
                 </div>
             </section>
             <Footer/>
-        </>
+        </section>
     )
 }
 
